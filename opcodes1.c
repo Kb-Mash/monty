e829dc67e84d4719c5ac114e1bc19172cf914662
Kb-Mash <karabo.kmashile@gmail.com>
2023-06-25 09:49:22 -0700

    sub update

--- opcodes1.c
+++ opcodes1.c
@@ -31,3 +31,25 @@ void nop(stack_t **stack, unsigned int line_number)
 	(void)stack;
 	(void)line_number;
 }
+
+/**
+ * sub - subtracts the top element of the stack from
+ * the second top element of the stack
+ * @stack: the stack
+ * @line_number: current working line number in the file
+ */
+void sub(stack_t **stack, unsigned int line_number)
+{
+	stack_t *top = *stack, *secondTop = NULL;
+
+	if (top == NULL || top->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't sub, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	secondTop = top->next;
+	secondTop->n -= top->n;
+
+	pop(stack, line_number);
+}
